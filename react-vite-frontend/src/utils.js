fab16e92b1e61d898201ee81ba39971a5e789af8
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-17 18:46:49 -0800

    created getImageURL func

---
+++ react-vite-frontend/src/utils.js
@@ -0,0 +1,5 @@
+// Takes a relative path that is passed to it and
+// appends it to the base URL of the website.
+export const getImageUrl = (path) => {
+  return new URL(`assets/${path}`, import.meta.url).href;
+};
