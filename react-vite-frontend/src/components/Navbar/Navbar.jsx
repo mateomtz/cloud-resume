9e1bbaf8b30a5f241a3fafc33466237993c9c923
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-17 19:07:27 -0800

    added secitons to Navbar list

--- react-vite-frontend/src/components/Navbar/Navbar.jsx
+++ react-vite-frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <a href="/">Portfolio</a>
+      <div>
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
