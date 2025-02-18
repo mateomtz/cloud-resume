b22aa0b07b496ff31c31262c1a6cc40447a21881
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-18 15:04:19 -0800

    added html and majority of css formatting

--- react-vite-frontend/src/components/Hero/Hero.jsx
+++ react-vite-frontend/src/components/Hero/Hero.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import heroImage from "../../assets/hero/heroImage.png";
+import styles from "./Hero.module.css"
 
 export const Hero = () => {
   return (
-    <section>
-        <div>
-            <h1>Hi, I'm Mateo.</h1>
-            <p>I'm an IT Support professional with 3 years of experience supporting SaaS products and macOS and Windows systems.
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hi, I'm Mateo.</h1>
+            <p className={styles.description}>I'm an IT Support professional with 3 years of experience supporting SaaS products and macOS and Windows systems.
                 Reach out on my LinkedIn if you'd like to learn more.
             </p>
-            
         </div>
+        <img src={heroImage} alt="hero image of me" className={styles.heroImg}/>
+            <div className={styles.topBlur}></div>
+            <div className={styles.bottomBlur}></div>
     </section>
   )
     
