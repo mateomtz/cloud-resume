44d7d00a1e29aa1e5c4ea6a26244f435076982f4
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-18 12:36:56 -0800

    implemented navbar styling and functionality

--- react-vite-frontend/src/components/Navbar/Navbar.jsx
+++ react-vite-frontend/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,11 @@ import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 import menuIcon from "../../assets/nav/menuIcon.png";
+import closeIcon from "../../assets/nav/closeIcon.png";
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
@@ -13,10 +16,12 @@ export const Navbar = () => {
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={menuIcon}
+          src={menuOpen ? closeIcon: menuIcon}
           alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
         />
-        <ul className={styles.menuItems}>
+        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={() => setMenuOpen(false)}>
           <li>
             <a href="#about">About</a>
           </li>
