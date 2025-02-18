be42d376018fd5eea40f709565dff3a0c34e3a3e
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-17 18:55:03 -0800

    imported Navbar into App.jsx

--- react-vite-frontend/src/App.jsx
+++ react-vite-frontend/src/App.jsx
@@ -1,8 +1,12 @@
-import styles from './App.module.css'
+import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-
-  return <div className={styles.App}>Hello world!</div>;
+  return (
+    <div className={styles.App}>
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
