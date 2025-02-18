c000c29a0b3782920c6fac872322233494dc321c
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-18 14:44:22 -0800

    created Hero component and css, imported to app.jsx as well

---
+++ react-vite-frontend/src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section>
+        <div>
+            <h1>Hi, I'm Mateo.</h1>
+            <p>I'm an IT Support professional with 3 years of experience supporting SaaS products and macOS and Windows systems.
+                Reach out on my LinkedIn if you'd like to learn more.
+            </p>
+            
+        </div>
+    </section>
+  )
+    
+}
