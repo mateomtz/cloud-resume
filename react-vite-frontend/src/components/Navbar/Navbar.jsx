27e4b541eb9c810bb0df58789e0d146470ba3da2
mateomtz <47834711+mateomtz@users.noreply.github.com>
2025-02-17 20:03:56 -0800

    added navbar component and added basic styling

--- react-vite-frontend/src/components/Navbar/Navbar.jsx
+++ react-vite-frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <a href="/">Portfolio</a>
-      <div>
-        <ul>
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        {/* change src to {getImageUrl("nav/menuIcon.png")}, for some reason it's not returning the */}
+        <img
+          className={styles.menuBtn}
+          src="/assets/nav/menuIcon.png"
+          alt="menu-button"
+        />
+        <ul className={styles.menuItems}>
           <li>
             <a href="#about">About</a>
           </li>
